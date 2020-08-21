85ea1025214eea24f6f8190d161ab11e22f47ddb
Ugo Varetto <ugovaretto@gmail.com>
2020-08-21 15:21:58 +0800

    Added fits hdu boundary extractor

---
+++ demo/data/extract-mwa-meta.py
@@ -0,0 +1,38 @@
+#!/bin/env python
+from astropy.io import fits
+import sys
+import json
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Extract HDUs boundaries as Ceph metadata")
+        print(f"usage: {sys.argv[0]} <id_data_device.fits file> <ceph bucket>",
+              file=sys.stderr)
+        sys.exit(1)
+    fits_file = sys.argv[1]
+    field_separator = '_'
+    id, date, device = fits_file.split(field_separator)[:3]
+    assert device  # check that there are indeed three fields
+    hdus = fits.open(fits_file)
+    header_info = []
+    for hdu in hdus:
+        fi = hdu.fileinfo()
+        hdr_start,  data_start, data_size = \
+            fi['hdrLoc'], fi['datLoc'], fi['datSpan']
+        header_info.append({"hdrLoc": hdr_start,
+                            "datLoc": data_start,
+                            "datSpan": data_size})
+    json_headers = {"hdu_boundaries": header_info}
+    meta_file = fits_file + ".bounds"
+    ceph_bucket = sys.argv[2] + "-bounds"
+    meta = {"x-amz-meta-id": id,
+            "x-amz-meta-date": date,
+            "x-amz-meta-device": device,
+            "x-amz-meta-bounds": meta_file,
+            "x-amz-meta-bounds-bucket": ceph_bucket}
+    with open(meta_file, "wb") as f:
+        f.write(bytes(json.dumps(json_headers), "utf-8"))        
+    for k, v in meta.items():
+        print(f"{k}:{v}", end=';')
+    print()
--- s3v4_rest.py
+++ s3v4_rest.py
@@ -554,6 +554,7 @@ def send_s3_request(config: Union[S3Config, str] = None,
         data = payload
         if parameters and not payload and req_method.lower() == 'post':
             data = parameters
+            #parameters = None
         response = _REQUESTS_METHODS[req_method.lower()](url=request_url,
                                                          data=data,
                                                          params=parameters,
